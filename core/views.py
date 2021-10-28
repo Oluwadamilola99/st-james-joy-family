772be635f9b7afa58f133554914b19596734b45f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-10-27 20:03:58 -0400

    Add comming soon page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Soon(TemplateView):
+    template_name = 'core/coming-soon.html'
